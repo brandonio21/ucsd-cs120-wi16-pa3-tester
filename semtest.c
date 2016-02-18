7afa5c438a0cc26de29a3d518d1eb0711845b34c
Brandon Milton <bmilton@ucsd.edu>
2016-02-18 10:02:08 -0800

    MADE TESTS ACTUALLY RUN. Added 2 more tests and simplified keeping track of the last call so things are easier to read

--- semtest.c
+++ semtest.c
@@ -3,8 +3,9 @@
 #include "../aux.h"
 #include "../sys.h"
 #include <stdio.h>
+#include <string.h>
 
-#define CALLS 10
+#define CALLS 100
 #define BLOCK 0
 #define UNBLOCK 1
 
@@ -13,17 +14,19 @@ struct call {
   int valid;
   int calledFunction;
   int arg;
-} callArr[CALLS];
+} lastCall;
 int failures = 0;
 int testFailures = 0;
 int tests = 0;
 
+
+/* debug output */
+int debug = 0;
+
 /* Blank out everything regarding the tester */
 void initTester() {
-  for (int i = 0; i < CALLS; i++) {
-    callArr[i].valid = 0;
-    callArr[i].calledFunction = -1;
-  }
+  lastCall.valid = 0;
+  lastCall.calledFunction = -1;
   failures = 0;
 }
 
@@ -39,96 +42,145 @@ void Printf(char *fmt, ...) {
 
 
 int Unblock(int p) {
-  for (int i = 0; i < CALLS; i++) {
-    if (!callArr[i].valid) {
-      callArr[i].valid = 1;
-      callArr[i].calledFunction = UNBLOCK;
-      callArr[i].arg = p;
-      return 0;
-    }
-  }
+  if (debug)
+    printf("[DEBUG] [USER] Unblock called with %d\n",p);
+
+  lastCall.valid = 1;
+  lastCall.calledFunction = UNBLOCK;
+  lastCall.arg = p;
 }
 
 int Block(int p) {
-  for (int i = 0; i < CALLS; i++) {
-    if (!callArr[i].valid) {
-      callArr[i].valid = 1;
-      callArr[i].calledFunction = BLOCK;
-      callArr[i].arg = p;
-      return 0;
-    }
-  }
+  if (debug)
+    printf("[DEBUG] [USER] Block called with %d\n",p);
+
+  lastCall.valid = 1;
+  lastCall.calledFunction = BLOCK;
+  lastCall.arg = p;
 }
 
 
 /* Tester functions to assert Block/Unblock calls */
 void assert_block_called_with(int param) {
-  int called = 0;
-  for (int i = CALLS-1; i >= 0; i--) {
-    if (callArr[i].valid && callArr[i].calledFunction == BLOCK) {
-      called = 1;
-      if (callArr[i].arg != param) {
-        printf(">ERR: Expected Block Call with %d but called with %d\n",param,
-            callArr[i].arg);
-        failures++;
-      }
+  if (debug)
+    printf("[DEBUG] Expecting last call to be Block with %d\n", param);
+  if (lastCall.valid && lastCall.calledFunction == BLOCK && lastCall.arg) {
+    if (lastCall.arg != param) {
+      printf(">ERR: Expected Block call with %d but called with %d\n",param,
+          lastCall.arg);
+      failures++;
     }
-  }
-  if (!called) {
-    printf(">ERR: Expected Block call with %d but not called\n",param);
+  } else {
+    printf(">ERR: Expected Block call with %d but never called\n", param);
     failures++;
   }
+  lastCall.valid = 0;
 }
 
 void assert_unblock_called_with(int param) {
-  int called = 0;
-  for (int i = CALLS-1; i >= 0; i--) {
-    if (callArr[i].valid && callArr[i].calledFunction == UNBLOCK) {
-      called = 1;
-      if (callArr[i].arg != param) {
-        printf(">ERR: Expected UnBlock Call with %d but called with %d\n",param,
-            callArr[i].arg);
-        failures++;
-      }
+  if (debug)
+    printf("[DEBUG] Expecting last call to be Unblock with %d\n", param);
+  if (lastCall.valid && lastCall.calledFunction == UNBLOCK && lastCall.arg) {
+    if (lastCall.arg != param) {
+      printf(">ERR: Expected Unblock call with %d but called with %d\n",param,
+          lastCall.arg);
+      failures++;
     }
-  }
-  if (!called) {
-    printf(">ERR: Expected Unblock call with %d but not called\n",param);
+  } else {
+    printf(">ERR: Expected Unblock call with %d but never called\n", param);
     failures++;
   }
+  lastCall.valid = 0;
 }
 
 
 void assert_block_not_called_with() {
-  for (int i = CALLS-1; i >= 0; i--) {
-    if (callArr[i].valid && callArr[i].calledFunction == BLOCK)  {
-      printf(">ERR: NonExpected Block Call called with %d\n", callArr[i].arg);
-      failures++;
-    }
+  if (debug)
+    printf("[DEBUG] Expecting last call to NOT BE Block\n");
+
+  if (lastCall.valid && lastCall.calledFunction == BLOCK) {
+    printf(">ERR: Did not expect Block call, but called with %d\n", 
+        lastCall.arg);
+    failures++;
   }
 }
 
 
 void assert_unblock_non_called_with() {
-  for (int i = CALLS-1; i >= 0; i--) {
-    if (callArr[i].valid && callArr[i].calledFunction == UNBLOCK) {
-      printf(">ERR: NonExpected Unblock Call called with %d\n", callArr[i].arg);
-      failures++;
-    }
+  if (debug)
+    printf("[DEBUG] Expecting last call to NOT BE Unblock\n");
+
+  if (lastCall.valid && lastCall.calledFunction == UNBLOCK) {
+    printf(">ERR: Did not expect Unblock call, but called with %d\n", 
+        lastCall.arg);
+    failures++;
   }
 }
 
 /* Test function that outputs small summary */
 void test(void (*testfunction)(), char* testName) {
+  if (debug) {
+    printf("---------------------------------------------\n");
+    printf("STARTING %s TEST\n",testName);
+  }
   invalidate();
   tests++;
+  testfunction();
   printf("%s: ", failures == 0 ? "PASS" : "FAIL");
   printf("%d %s test failures\n",failures,testName);
   if (failures > 0)
     testFailures++;
+  if (debug)
+    printf("---------------------------------------------\n");
 }
 
 /* TESTS GO HERE ------------------- */
+void test_fifo_impl() {
+  int s = MySeminit(0,0);
+  MyWait(1,s);
+  assert_block_called_with(1);
+  MyWait(2,s);
+  assert_block_called_with(2);
+  MyWait(3,s);
+  assert_block_called_with(3);
+  MyWait(4,s);
+  assert_block_called_with(4);
+  MyWait(5,s);
+  assert_block_called_with(5);
+  MyWait(6,s);
+  assert_block_called_with(6);
+  MySignal(0,s);
+  assert_unblock_called_with(1);
+  MySignal(0,s);
+  assert_unblock_called_with(2);
+  MySignal(0,s);
+  assert_unblock_called_with(3);
+  MySignal(0,s);
+  assert_unblock_called_with(4);
+  MySignal(0,s);
+  assert_unblock_called_with(5);
+  MySignal(0,s);
+  assert_unblock_called_with(6);
+  MySignal(0,s);
+  assert_unblock_non_called_with();
+}
+void high_value_sem() {
+  int s = MySeminit(0,5);
+  MyWait(1,s);
+  assert_block_not_called_with();
+  MyWait(2,s);
+  assert_block_not_called_with();
+  MyWait(3,s);
+  assert_block_not_called_with();
+  MyWait(4,s);
+  assert_block_not_called_with();
+  MyWait(5,s);
+  assert_block_not_called_with();
+  MyWait(6,s);
+  assert_block_called_with(6);
+  MySignal(0,s);
+  assert_unblock_called_with(6);
+}
 void nonlocked_sem_test() {
   int s = MySeminit(1,1);
   MyWait(1,s);
@@ -148,6 +200,7 @@ void locked_sem_test_all_wait() {
   MyWait(5,s);
   assert_block_called_with(5);
 }
+
 void locked_sem_test() {
   int s = MySeminit(1,1);
   MyWait(1,s);
@@ -159,11 +212,15 @@ void locked_sem_test() {
 }
 
 /* TESTS ARE CALLED HERE ----------- */
-void main() {
+void main(int argc, char** argv) {
+  if (argc > 1 && strcmp(argv[1], "-v") == 0)
+    debug = 1;
   InitSem();
   test(nonlocked_sem_test, "Simple non-wait");
   test(locked_sem_test_all_wait, "Zero-val semaphore 5 procs");
   test(locked_sem_test, "2 proc must wait");
+  test(high_value_sem, "High value semaphore");
+  test(test_fifo_impl, "FIFO Test");
 
   /* final output */
   printf("%d/%d TESTS PASSED\n", tests-testFailures, tests);
