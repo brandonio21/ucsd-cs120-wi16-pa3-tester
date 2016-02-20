a2066dd3b3de2b342b5adb5978b4aa251f866108
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-02-19 20:38:03 -0800

    Added test for allocating MAXSEMS semaphores

--- semtest.c
+++ semtest.c
@@ -8,6 +8,14 @@
 #define BLOCK 0
 #define UNBLOCK 1
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
 /* Used to keep track of the previous CALLS Unblock/Block calls */
 struct call {
   int valid;
@@ -115,6 +123,40 @@ void assert_unblock_not_called_with() {
         lastCall.arg);
     failures++;
   }
+}
+
+void fail(char* reason) {
+    failures++;
+    if (debug) {
+        printf("fail() fail: %s\n", reason);
+    }
+}
+
+void assert_true(int expr, char* reason) {
+    if (!expr) {
+        failures++;
+        if (debug) {
+            printf("assert_true() fail: %s\n", reason);
+        }
+    }
+}
+
+void assert_false(int expr, char* reason) {
+    if (expr) {
+        failures++;
+        if (debug) {
+            printf("assert_false() fail: %s\n", reason);
+        }
+    }
+}
+
+void assert_equals(int expected, int actual, char* reason) {
+    if (expected != actual) {
+        failures++;
+        if (debug) {
+            printf("assert_equals() expected %d, got %d: %s\n", expected, actual, reason);
+        }
+    }
 }
 
 /* Test function that outputs small summary */
@@ -274,6 +316,64 @@ void test_fifo_no_starvation() {
     }
 }
 
+/*
+ * Attempts to allocate MAXSEMS semaphores in a loop, verifying
+ * that no semaphore id is reused.
+ * Finally verifies that once MAXSEMS semaphores have been allocated,
+ * additional calls to MySeminit() return an invalid semaphore id (-1).
+ *
+ * NOTE: InitSem() must be called immediately before this test is run
+ * so that semaphores allocated by previous tests are freed up.
+ */
+void test_allocate_all_semaphores() {
+    // "boolean" array of the semaphore ids allocated so far
+    int allocated_sems[MAXSEMS];
+    for (int i = 0; i < MAXSEMS; i++) {
+        allocated_sems[i] = FALSE;
+    }
+    int count_allocated = 0;
+
+    char buf[80];
+    for (int i = 0; i < MAXSEMS; i++) {
+        int sem = MySeminit(1, 0);
+
+        // fail if the semaphore id is out of bounds
+        if (sem < 0 || sem >= MAXSEMS) {
+            sprintf(buf, "semaphore id out of bounds: %d (%dth iteration)", sem, i);
+            fail(buf);
+            break;
+        }
+
+        sprintf(buf, "Already allocated semaphore id %d (%dth iteration)", sem, i);
+        // fail if we already allocated this semaphore id
+        assert_false(allocated_sems[sem], buf);
+
+        if (!allocated_sems[sem]) {
+            count_allocated++;
+        }
+
+        // mark this semaphore id as acquired
+        allocated_sems[sem] = TRUE;
+    }
+
+    if (count_allocated != MAXSEMS) {
+        if (debug) {
+            printf("Only allocated %d out of %d semaphore ids\n", count_allocated, MAXSEMS);
+            printf("Missed Ids: ");
+            for (int sem = 0; sem < MAXSEMS; sem++) {
+                if (!allocated_sems[sem]) {
+                    printf("%d ", sem);
+                }
+            }
+            printf("\n");
+        }
+    }
+
+    // try to allocate one last semaphore (which should be -1)
+    int sem = MySeminit(1, 0);
+    assert_equals(-1, sem, "Allocating last semaphore");
+}
+
 /* TESTS ARE CALLED HERE ----------- */
 void main(int argc, char** argv) {
   if (argc > 1 && strcmp(argv[1], "-v") == 0)
@@ -287,6 +387,11 @@ void main(int argc, char** argv) {
   test(test_max_wait_list_fifo, "Maxing out process wait list");
   test(test_repeated_all_wait_all_signal, "Repeated all wait / all signal");
   test(test_fifo_no_starvation, "Cyclic wait signal one at a time");
+
+  // need to clear all allocated semaphores in order to allocate them in this test
+  InitSem();
+  test(test_allocate_all_semaphores, "Allocate MAXSEMS semaphores");
+  InitSem();
 
   /* final output */
   printf("%d/%d TESTS PASSED\n", tests-testFailures, tests);
